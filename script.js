19c84fe71435654cc863564626a6646373c82a50
salma <saidisalma594@gmail.com>
2022-09-12 09:34:34 +0100

    calculate

--- script.js
+++ script.js
@@ -1,12 +1,53 @@
-let input,add,list;
-input = document.querySelector("input");
-add = document.querySelector("button");
+let affichage = document.getElementById("affichage");
+ let btn1 = document.getElementById("control");
+ let btn2 = document.getElementById("reje");
+ let saisie = document.querySelector("input");
+ let mot = document.getElementById("fin");
+ let zoneA = document.getElementById("jeu");
+ let zoneB =document.getElementById("finJeu");
 
-add.addEventListener("click", function(){
-    console.log(input.value);
-    let listItem = document.createElement("li");
-    listItem.textContent= input.value;
-    let ul = document.querySelector("ul");
-    ul.appendChild(listItem);
-    input.value= "";
-});
+ function Findejeu(){
+  zoneA.style.display="none";
+  zoneB.style.display="block";
+ }
+ function Rejouer(){
+  Somme();
+  zoneB.style.display="none";
+  zoneA.style.display="block"
+ }
+
+ function Aleatoire(min, max)
+ {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+let entierA;
+ let entierB;
+let res ;
+function Somme() {
+  entierA = Aleatoire(1, 100);
+  entierB = Aleatoire(1, 100);
+  res = entierA+entierB;
+ affichage.innerText = entierA+" "+"+"+" "+entierB;
+ affichage.style.color="black";
+ console.log(res);
+}
+
+function Compariason(){
+
+  if(saisie.value.length<=0){
+    mot.innerText= "Box empty choose a number";
+    mot.style.color="blue";
+  }else if(saisie.value == res){
+    mot.innerText= "Excellent you won";
+    mot.style.color="green";
+  }
+  else{
+    mot.innerText= "You lost";
+    mot.style.color="red";
+  }
+  Findejeu();
+  saisie.value="";
+};
+ Somme();
+ btn1.addEventListener("click",Compariason);
+ btn2.addEventListener("click",Rejouer);
