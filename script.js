316af3504d9fd9800e74a11f6288d7b4fd0e7f29
salma <saidisalma594@gmail.com>
2022-09-14 10:04:50 +0100

    vhf

--- script.js
+++ script.js
@@ -1,22 +1,2 @@
-// let form=document.querySelector("form");
-// let nom=form.elements['nom'];
-// let pnom=form.elements['prenom'];
-// console.log(nom.value);
-// console.log(pnom.value);
-// let char=/^[a-zA-Z-\s]$/;
-// form.addEventListener('submit',function(event){
-//     event.preventDefault();
-//     if(nom.value.length==0){
-//         let error=document.getElementById("error");
-//         error.innerText="The box is obligatory please fill in the box";
-//         error.style.color="red";
-//     }else if (char.test(nom.value)=false){
-//         error.innerText="The box is obligatory please fill in the box";
-//         error.style.color="red";
-//     }
-// })
 
-// let email="Salma4s@gmail.com"
-// let char = /^[a-zA-Z0-9-]+@[a-zA-Z]+\.[a-z]+$/;
-// console.log(char.test(email));
 
