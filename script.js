b6f7817ef15da52beb73ba1ad9f17124885f516e
salma <saidisalma594@gmail.com>
2022-09-14 01:54:06 +0100

    calculator html and css

--- script.js
+++ script.js
@@ -20,24 +20,3 @@
 // let char = /^[a-zA-Z0-9-]+@[a-zA-Z]+\.[a-z]+$/;
 // console.log(char.test(email));
 
-let form = document.querySelector("form");
-let pwd=form.elements['pwd'];
-
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-  let inputs = form.elements;
-  let invMess = document.querySelector(".invalid_password");
-  if (inputs["pwd"].value.length == 0) {
-    inputs["pwd"].className = "is_invalid";
-    invMess.textContent = "Please Enter a Password";
-  } else {
-    inputs["pwd"].className = "is_valid";
-    invMess.textContent = "";
-    // recuperation de la valeur saisi qui est valide
-    const pwd = inputs["pwd"].value;
-    console.log(pwd);
-  }
-});
-
-console.log(pwd.value);
-let char=/^[a-zA-Z0-9._-]{6,12}$/;
